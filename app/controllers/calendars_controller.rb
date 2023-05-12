17f210fbd4a8d35f4956dd6499d6534b5c606e17
koharayuki <aquarius3106@icloud.com>
2023-05-12 17:00:22 +0900

    Issue6
    ・index.html.erbに”曜日の情報”を出力するための記述を追加
    
    ・calendars_controller.rbにビューファイルへ”曜日の情報”を送る記述を追加

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,11 +15,11 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:plan).permit(:date, :plan)
+    params.require(:plan).permit(:date, :plan,)
   end
 
   def getWeek
-    wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
+
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
@@ -29,12 +29,23 @@ class CalendarsController < ApplicationController
 
     plans = Plan.where(date: @todays_date..@todays_date + 6)
 
+
     7.times do |x|
       today_plans = []
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+
+
+      wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
+      @day = Date.today.wday
+      # @wdays =wdays[@day+x]
+
+      if @day+x >= 7
+        @day -=7
+      end
+
+      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :wdays => wdays[@day+x]}
       @week_days.push(days)
     end
 
